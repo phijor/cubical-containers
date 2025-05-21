2f59ab9bcaa99a98331b7b52f323587680b6b144
Philipp Joram <mail@phijor.me>
2025-05-21 12:05:12 +0300

    Depend on `cubical` v0.8 from `nixpkgs`
    With the release of a stable version of `cubical` for Agda 2.7.*,
    we can remove the dependency on the development version of the library.

--- flake.nix
+++ flake.nix
@@ -3,20 +3,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    cubical = {
-      url = "github:agda/cubical";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
-      cubical,
       ...
     }:
     let
@@ -25,15 +17,13 @@
     eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cubical.overlays.default ];
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
+        cubical = pkgs.agdaPackages.cubical;
         cubical-categorical-logic = pkgs.callPackage ./cubical-categorical-logic.nix {
-          inherit (pkgs) cubical;
+          inherit cubical;
         };
         groupoid-containers = pkgs.callPackage ./groupoid-containers.nix {
-          inherit (pkgs) cubical;
+          inherit cubical;
           inherit cubical-categorical-logic;
         };
       in
